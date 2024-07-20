2ff2ab2fcf5a4d1968acb3b50388c8ae5f56e8d8
RaymondHornDev <132532625+RaymondHornDev@users.noreply.github.com>
2024-07-20 02:02:34 -0400

    Update

--- BST/BST.py
+++ BST/BST.py
@@ -15,7 +15,8 @@ class Tree:
         if self.root is None:
             self.root = Node(value)
         else:
-            pass
+            self.recursive_add(self.root, value)
+        
     def recursive_add(self, node, value):
         if node.value > value:
             if node.left is None:
@@ -49,3 +50,21 @@ class Tree:
         if node.right is not None:
             self.post_order(node.right)
         print(node.value)
+        
+def fill(list, value, tree):
+    if value < len(list):
+        tree.insert(list[value])
+        fill(list, value + 1, tree)
+        
+
+        
+if __name__ == "__main__":
+    my_list = [5,4,3,2,1,0,6,7,8,9]
+    tree = Tree()
+    
+    fill(my_list, 0, tree)
+    
+    tree.in_order(tree.root)
+    tree.pre_order(tree.root)
+    tree.post_order(tree.root)
+    
