1f60b6db7afe1cc6bbbf3b1da26a0fec394e9820
RaymondHornDev <132532625+RaymondHornDev@users.noreply.github.com>
2024-07-30 21:43:04 -0400

    Update

--- SortList/SortList.py
+++ SortList/SortList.py
@@ -11,3 +11,21 @@ class List:
     def __init__(self) -> None:
         self.head = None
         self.has_nodes = False
+        
+    def push(self, value):
+        if self.head is None:
+            self.head = Node(value)
+        else:
+            pass
+    def recursive_add(node, value):
+        if node.value < value:
+            loc_node = Node(value)
+            
+            loc_node.prev = node
+            node.next = loc_node
+        else:
+            loc_node = Node(value)
+            
+            loc_node.next = node
+            node.prev = loc_node
+            
